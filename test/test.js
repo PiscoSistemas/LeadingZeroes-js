f188a477228f92dfb4d31371c7cf10398564ce82
Vitor <vitor83dev@gmail.com>
2018-06-18 19:30:20 -0300

    Initial tests commit

---
+++ test/test.js
@@ -0,0 +1,5 @@
+require('../index');
+console.log("12345".leadingZeroes(10)); // >>> "0000012345"
+console.log("1".leadingZeroes(2)); // >>> "01"
+console.log("50".leadingZeroes(2)); // >>> "50"
+console.log("ABC".leadingZeroes(4)); // >>> "0ABC"
